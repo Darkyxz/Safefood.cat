7d89c6539bd34e4206efa8c8c83c3e0b554180a7
daniel leon <98672399+Darkyxz@users.noreply.github.com>
2024-10-19 14:47:23 -0300

    Create compendium.js

---
+++ js/compendium.js
@@ -0,0 +1,33 @@
+const COMPENDIUM = [
+	{
+		name: "Bananas",
+		safety: 3,
+		details: "Yes, Felines require potassium for good health; otherwise, they can develop health problems like lethargy, weight loss, muscle pain, and even muscle weakness.",
+		source: {
+			name: "catster",
+			url: "https://www.catster.com/nutrition/fruits-and-vegetables-cats-can-eat/",
+			date: 1725302703,
+		}
+	},
+	{
+		name: "Cantaloupe",
+		safety: 3,
+		details: "Yes, There are many vitamins and minerals present in cantaloupe that cats could benefit from, but it’s unclear how many of these nutrients they actually absorb when eating the fruit, given their carnivorous tendencies.",
+		source: {
+			name: "catster",
+			url: "https://www.catster.com/nutrition/fruits-and-vegetables-cats-can-eat/",
+			date: 1725302703,
+		}
+	},
+	{
+		name: "Mustard",
+		safety: 1,
+		details: "No, Vinegar and mustard seeds in table mustard aren’t necessarily toxic to cats, but they can have adverse effects when your pet eats them in excess. Although vinegar may have some benefits, the acidity could cause an upset stomach.",
+		source: {
+			name: "catster",
+			url: "https://www.catster.com/nutrition/can-cats-eat-mustard/",
+			date: 1725302703,
+		}
+	}
+  
+]
